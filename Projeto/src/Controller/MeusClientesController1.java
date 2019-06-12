f6bef8eed2c63d0a867380ff1a39450c562e80b6
Romulo Sobreira <romurevoltado@gmail.com>
2019-06-11 21:53:52 -0300

    projetov1
    Co-Authored-By: João Victor Queiroz de Souza <jvyctor12@users.noreply.github.com>
    Co-Authored-By: senideia <senideia@users.noreply.github.com>
    Co-Authored-By: oliveirapaula <oliveirapaula@users.noreply.github.com>

---
+++ Projeto/src/Controller/MeusClientesController1.java
@@ -0,0 +1,126 @@
+package Controller;
+
+import Main.Main;
+import Modelo.ModeloUsuario;
+import ModeloConection.ConnectionFactory;
+import ModeloDao.DaoUsuario;
+import ModeloDao.UsuarioDao;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+
+public class MeusClientesController1{
+    ModeloUsuario mod = new ModeloUsuario();
+    UsuarioDao control = new UsuarioDao();
+    ConnectionFactory conex = new ConnectionFactory();
+    
+    @FXML
+    private Parent nova;
+    @FXML
+    private TextField buscarTextField;
+    
+    @FXML
+    private Button excluirButton;
+
+    @FXML
+    private Button inicioButton;
+    @FXML    private TableView<ModeloUsuario> meusClientestableView;
+    @FXML    private TableColumn<ModeloUsuario, Integer> codClientestableView;
+    @FXML    private TableColumn<ModeloUsuario,String> emailClientestableView;
+    @FXML    private TableColumn<ModeloUsuario, String> tefefoneClientestableView;
+    @FXML    private TableColumn<ModeloUsuario, String> enderecoClientestableView;
+    @FXML    private TableColumn<ModeloUsuario, String> obsClientestableView;
+    @FXML    private TableColumn<ModeloUsuario, String> nomeClientestableView;
+    @FXML    private TableColumn<ModeloUsuario, String> cpfClientestableView;
+    
+    @FXML
+    private Button atualizarButton;
+    @FXML
+    private Button enfButton;
+
+    @FXML
+    private Button sairButton;
+
+    @FXML
+    private Button voltarButton;
+
+    @FXML
+    private Button buscarButton;
+          
+   
+   public void initialize(URL url, ResourceBundle rb){
+        initable();
+   }
+   @FXML
+    void inicioButtonAction(ActionEvent event){
+        try {
+                 nova= FXMLLoader.load(getClass().getResource("/View/MenuPrincipal.fxml"));
+            Main.trocarTela(nova);
+            } catch (IOException ex) {
+                Logger.getLogger(MeusClientesController1.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+    @FXML
+    void voltarButtonAction(ActionEvent event){
+        try {
+                 nova= FXMLLoader.load(getClass().getResource("/View/GerenciamentoCliente.fxml"));
+            Main.trocarTela(nova);
+            } catch (IOException ex) {
+                Logger.getLogger(MeusClientesController1.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+    @FXML
+    void sairButtonAction(ActionEvent event){
+       try {
+                 nova= FXMLLoader.load(getClass().getResource("/Viw/Login.fxml"));
+            Main.trocarTela(nova);
+            } catch (IOException ex) {
+                Logger.getLogger(MeusClientesController1.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+    @FXML
+    void buscarButtonAction(ActionEvent event){
+       /*mod.setPesquisa(buscarTextField.getText());
+       ModeloUsuario model = control.buscaCliente(mod);
+       mod.setNome(model.getNome());
+       mod.setTelefone(model.getTelefone());
+       mod.setCpf(model.getCpf());
+       mod.setEmail(model.getEmail());
+       mod.setCidade(model.getCidade());
+       mod.setBairro(model.getBairro());
+       mod.setComplemento(model.getComplemento());
+       mod.setEndereco(model.getEndereco());
+       mod.setN(model.getN());*/
+    }
+    @FXML
+    void excluirButtonAction(ActionEvent event){
+       //é aqui romulo
+    }
+    public void initable(){
+        codClientestableView.setCellValueFactory(new PropertyValueFactory("id"));
+        nomeClientestableView.setCellValueFactory(new PropertyValueFactory("nome"));
+        tefefoneClientestableView.setCellValueFactory(new PropertyValueFactory("telefone"));
+        cpfClientestableView.setCellValueFactory(new PropertyValueFactory("cpf"));
+        emailClientestableView.setCellValueFactory(new PropertyValueFactory("email"));  
+        enderecoClientestableView.setCellValueFactory(new PropertyValueFactory("endereco"));
+        meusClientestableView.setItems(atualizaTabela());
+    }
+    public ObservableList<ModeloUsuario> atualizaTabela(){
+       DaoUsuario dao = new DaoUsuario();
+       return FXCollections.observableArrayList(dao.getList());
+    }
+}
