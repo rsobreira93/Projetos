d561367fe64c03a4aba43d36f93c4f46820672eb
Romulo Sobreira <romurevoltado@gmail.com>
2019-06-25 12:42:28 -0300

    V1.1
    Com recuperação de senha do usuario implementado
    
    Co-Authored-By: João Victor Queiroz <jvqueiroz@users.noreply.github.com>
    Co-Authored-By: João Victor Queiroz de Souza <jvyctor12@users.noreply.github.com>
    Co-Authored-By: oliveirapaula <oliveirapaula@users.noreply.github.com>
    Co-Authored-By: senideia <senideia@users.noreply.github.com>

---
+++ Projetos-master/Projeto/src/Controller/EsqueciSenhaController.java
@@ -0,0 +1,51 @@
+package Controller;
+
+import Main.Main;
+import java.io.IOException;
+import static java.sql.Types.NULL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import java.util.Random;
+public class EsqueciSenhaController{
+    private Parent nova;
+    @FXML
+    private Label IncorretoLabel;
+    @FXML
+    private Button ButtonRecuperar;
+    @FXML
+    private TextField EmailTextField;
+    @FXML
+    private Button ButtonCancelar;
+    @FXML
+    void ButtonRecuperarAction(ActionEvent event){
+        //verifica se o email existe no banco de dados
+        if(EmailTextField.getText().equals("jvyctor12@gmail.com")){
+           Random novaSenha = new Random();
+           //muda a senha lá no bando de dados
+           Main.emailEsqueciSenha("jvyctor12@gmail.com",novaSenha.nextInt());
+            IncorretoLabel.setText("Sua nova senha foi enviada para seu e-mail");
+      }
+        else if(EmailTextField.getText().equals(NULL))
+            IncorretoLabel.setText("Digite seu e-mail");
+        else
+            IncorretoLabel.setText("Esse e-mail não pertence a nenhum usuário");
+    }
+    @FXML
+    void ButtonCancelarAction(ActionEvent event){
+        try {
+            nova= FXMLLoader.load(getClass().getResource("/View/Login.fxml"));
+            Main.trocarTela(nova);
+            } catch (IOException ex) {
+                Logger.getLogger(EsqueciSenhaController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+ 
+    }
+
---
+++ Projetos-master/Projeto/src/Controller/MenuPrincipalController.java
@@ -0,0 +1,79 @@
+
+package Controller;
+
+import Main.Main;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.LoadException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+public class MenuPrincipalController {
+    private Parent nova;
+    @FXML
+    private Button aClientesButton;
+
+    @FXML
+    private Button gProdutosButton;
+
+    @FXML
+    private Button financasButton;
+
+    @FXML
+    private Button enfButton;
+
+    @FXML
+    private Button sairButton;
+
+    @FXML
+    private Button contaButton;
+
+    @FXML
+    private Button gClientesButton;
+
+    @FXML
+    private Button gVendasButton;
+
+    @FXML
+    void sairButtonAction(ActionEvent event){
+        try {
+            nova= FXMLLoader.load(getClass().getResource("/View/Login.fxml"));
+            Main.trocarTela(nova);
+            } catch (IOException ex) {
+                Logger.getLogger(MenuPrincipalController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    @FXML
+    void gClientesButtonAction(ActionEvent event) {
+          try {
+                 nova= FXMLLoader.load(getClass().getResource("/View/GerenciamentoCliente.fxml"));
+                 Main.trocarTela(nova);
+            } catch (IOException ex) {
+                Logger.getLogger(MenuPrincipalController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+    @FXML
+    void gProdutosButtonAction(ActionEvent event){
+        try {
+                 nova= FXMLLoader.load(getClass().getResource("/View/GerenciamentoProduto.fxml"));
+                 Main.trocarTela(nova);
+            } catch (IOException ex) {
+                Logger.getLogger(MenuPrincipalController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+    @FXML
+    void contaButtonAction(ActionEvent envent){
+        try {
+                 nova= FXMLLoader.load(getClass().getResource("/View/ContaUser.fxml"));
+                 Main.trocarTela(nova);
+            } catch (IOException ex) {
+                Logger.getLogger(MenuPrincipalController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+}
+    
+
+
